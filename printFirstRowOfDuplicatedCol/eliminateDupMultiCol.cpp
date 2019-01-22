79a3cff505771edc42dc261189bdbb4750b21c3c
Guang Mo <gumo@microsoft.com>
2019-01-22 15:18:47 -0800

    introduce only print first row on a selected columns with duplicates

---
+++ printFirstRowOfDuplicatedCol/eliminateDupMultiCol.cpp
@@ -0,0 +1,63 @@
+// eliminateDupMultiCol.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// printNoDup.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <atlstr.h>
+#include <string>
+#include <list>
+
+int main()
+{
+    std::string line;
+    std::string inputFileName;
+    int colToNoDup;
+    std::list<CStringA> noDupList;
+    std::ifstream inputFile(inputFileName);
+
+    /* Get user inputs */
+    std::cout << "Please provide the name of input file and make sure it is at the current directory (eg: input.txt): "; 
+    std::cin >> inputFileName;
+    std::cout << "Please provide the index of column that you want to get rid of duplicates, starting from 1: "; 
+    std::cin >> colToNoDup; 
+
+    /* Read the input file and store distinct values in the list */
+    if (inputFile.is_open())
+    {
+        while (std::getline(inputFile, line))
+        {
+            if (!line.empty())
+            {
+                CStringA CLine = line.c_str();
+                int pos = 0;
+                CStringA colString;
+                for (int i = 0; i < colToNoDup; i++)
+                {
+                    colString = CLine.Tokenize(",", pos);
+                    /* handle case where the line is not normal */
+                    if (colString.IsEmpty())
+                    {
+                        break; 
+                    }
+                }
+
+                colString.Trim(); 
+                std::list<CStringA>::iterator findIter = std::find(noDupList.begin(), noDupList.end(), colString);
+                if (findIter == noDupList.end())
+                {
+                    noDupList.push_back(colString);
+                    std::cout << line << "\n"; 
+                }
+            }
+        }
+    }
+    else
+    {
+        std::cout << "The input file " << inputFileName << "can not open, exiting \n"; 
+    }
+
+    return 0;
+} 
---
+++ printFirstRowOfDuplicatedCol/readme.txt
@@ -0,0 +1,12 @@
+This program is intended to provide unique value for columns contain duplicated values. Users can specify which column for the duplicated columns. 
+and the program only works for csv files. 
+
+For example, the follow table: 
+
+11-1, Guang, abc.com 
+10-1, Nick, bbc.com 
+11-3, Guang, ddd.com 
+
+by running this problem, with the file as input, and pass 2 for the 2nd colum, the following will be printed out: 
+11-1, Guang, abc.com 
+10-1, Nick, bbc.com 
