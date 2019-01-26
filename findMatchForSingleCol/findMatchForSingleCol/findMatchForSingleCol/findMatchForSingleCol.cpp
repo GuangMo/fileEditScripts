12570360fd2b7b364ff3f9fc839475bb6e279be1
Guang Mo <gumo@microsoft.com>
2019-01-25 16:27:56 -0800

    added to provide option to display matches or unmatch entries

--- findMatchForSingleCol/findMatchForSingleCol/findMatchForSingleCol/findMatchForSingleCol.cpp
+++ findMatchForSingleCol/findMatchForSingleCol/findMatchForSingleCol/findMatchForSingleCol.cpp
@@ -14,12 +14,15 @@ int main()
     string line;
     string inputFileNameA;
     string inputFileNameB;
+    bool showMatch = false; 
     list<CStringA> inputsFromFileA; 
 
     cout << "Please provide the name of first input file and make sure it is at the current directory (eg: inputA.txt): ";
     cin >> inputFileNameA;
-    cout << "Please provide the name of 2nd input file and make sure it is at the current directory (eg: inputB.txt): ";;
+    cout << "Please provide the name of 2nd input file and make sure it is at the current directory (eg: inputB.txt): ";
     cin >> inputFileNameB;
+    cout << "Do you want to show the matches ? (default is to show the dismatches) 1/0: "; 
+    cin >> showMatch; 
 
     ifstream inputFileA(inputFileNameA);
     if (inputFileA.is_open())
@@ -48,9 +51,16 @@ int main()
             CStringA CLine = line.c_str();
             CLine.Trim();
             list<CStringA>::iterator findIter = find(inputsFromFileA.begin(), inputsFromFileA.end(), CLine);
-            if (findIter == inputsFromFileA.end())
+            if (findIter == inputsFromFileA.end() && !showMatch)
             {
-                cout << "The 2nd file " << inputFileNameA << " does not contain " << CLine << "\n";
+                cout << "The 2nd file " << inputFileNameB << " contains " << CLine << " that is not in the 1st file \n";
+            }
+            else
+            {
+                if (showMatch)
+                {
+                    cout << "The 2nd file " << inputFileNameB << " contains " << CLine << " that is also in 1st file \n";
+                }
             }
         }
     }
